fcfd9052c33d6d7064a836120196484f0a94089f
Jasurbek <jasurisoqov685@gmail.com>
2022-04-27 02:15:33 +0500

    create state

--- src/components/FiltarebleProductTable.js
+++ src/components/FiltarebleProductTable.js
@@ -6,13 +6,21 @@ import ProductTable from "./ProductTable.js";
 class FiltarebleProductTable extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      filterText: "",
+      inStockOnly: false,
+    };
   }
 
   render() {
     return (
       <div>
         <SearchBar />
-        <ProductTable products={this.props.product} />
+        <ProductTable
+          products={this.props.product}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
       </div>
     );
   }
